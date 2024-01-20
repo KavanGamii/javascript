06884904d249d17fcb55b6c06889440fcd76d36c
Kavan <156985241+KavanGamii@users.noreply.github.com>
2024-01-20 08:44:26 +0000

    done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = "33cxcx"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number (score)
+// console.log(valueInNumber);
+
+//"33 => 33"
+//33asa => NaN
+//true => 1 ; false =>0;
+
+// let isLoggedIn="1"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// //i =>true; 0 ==> false.
+// //""=> false
+// //"Kavan" ==> True
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
