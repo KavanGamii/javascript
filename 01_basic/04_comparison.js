26cd771998d21928aee2127aaae68252834ad98e
Kavan <156985241+KavanGamii@users.noreply.github.com>
2024-01-21 11:56:32 +0000

    done

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 4);
+// console.log(2 >= 4);
+// console.log(2 < 4);
+// console.log(2 == 2);
+// console.log(2 != 2);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+                                    // *************************Avoid Using this conversion********************//
+// console.log(undefined > 0);
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+console.log("1" == 1);
+
+// === check datatype
+console.log("1" === 1);
