3e45c7c1b0731e6746da387b0a8cf887652a83d2
Kavan <156985241+KavanGamii@users.noreply.github.com>
2024-01-22 12:24:37 +0000

    num and math

---
+++ 01_basic/06_num_and_maths.js
@@ -0,0 +1,36 @@
+// const score =400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 223.536
+
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+//****************Maths*******************//
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(3.6))
+// console.log(Math.ceil(3.4))
+// console.log(Math.floor(3.5))
+// console.log(Math.min(3, 4 ,4 ,5, 5,6))
+// console.log(Math.max(3, 4 ,4 ,5, 5,6))
+console.log(Math.random())
+console.log((Math.random()*9) +1)
+console.log((Math.random()*9) +1)
+console.log(Math.floor(Math.random()*9) +1)
+
+const min =10
+const max =20
+
+console.log(Math.random() * (max - min + 1) + min)
