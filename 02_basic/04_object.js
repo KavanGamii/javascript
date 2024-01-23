cc333e32f4c0000ad32bcd65259a38412e3cada6
Kavan <156985241+KavanGamii@users.noreply.github.com>
2024-01-23 12:46:42 +0000

    done

---
+++ 02_basic/04_object.js
@@ -0,0 +1,47 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.id = 21
+tinderUser.name = "Kavan"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser)
+
+const regualrUser ={
+    email : "kavangami.com",
+    fullname:{
+        userFullName:{
+            firstname:"Kavan",
+            lastnmae:"Gami"
+
+        }
+    }
+}
+// console.log(regualrUser.fullname);
+
+const obj1 = {1:"a", 2: "b"}
+const obj2 = {3:"c", 4: "d"}
+
+// const obj3 ={obj1 , obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        emailId: "Kavangami"
+    },
+    {
+        id: 2,
+        emailId: "Kavangami"
+    }
+]
+users[1].emailId
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
