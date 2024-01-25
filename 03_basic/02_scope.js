d4b5fe7206bd5c8dd49903825ed62c8fd7bbd843
Kavan <156985241+KavanGamii@users.noreply.github.com>
2024-01-25 05:42:37 +0000

    javascript

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,49 @@
+// let a = 300
+// if (true){
+//     let a = 10
+//     const b =2
+//     // console.log("inner", a);
+
+// }
+// // for (let i =i < array.lth ; ength ; i++) {
+// //     const element = araar[i];
+    
+// // }
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+
+// // ******Nested Scope
+
+// function one(){
+//     const usernames = "Kavan"
+//     function two(){
+//         const website = "Youtube"
+//         // console.log(usernames)
+//     }
+//     // console.log(website);
+//     two()
+// }
+// // one()
+// if (true){
+//     const usernames = "Kavan"
+//     if(usernames === "Kavan"){
+//         const website = "Youtube"
+//         // console.log(usernames + website)
+//     }
+//     // console.log(website);
+// }
+// // console.log(usernames);
+
+// +++++++++++++++++++++Intreasting +++++++++++++++++++++++
+console.log(addone(5));
+function addone(num){
+    return num + 1
+
+}
+
+addtwo(5)
+const addtwo = function(num){
+    return num +2
+}
